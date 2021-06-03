fc92cc7d8ace5b8d9dc80c5df0ed2e51405e3bcf
VasilyevaOlga <Vasilyeva.Olga.95@gmail.com>
2021-06-03 12:38:51 +0600

    lab5

---
+++ Backend5/Data/HospitalContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Backend5.Models;
+
+namespace Backend5.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public DbSet<HospitalModel> Hospitals { get; set; }
+        public DbSet<LabModel> Labs { get; set; }
+        public DbSet<DoctorModel> Doctors { get; set; }
+        public DbSet<PatientModel> Patients { get; set; }
+
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options)
+        {
+            Database.EnsureCreated(); 
+        }
+
+        public HospitalContext()
+        {
+
+        }
+    }
+}
---
+++ Backend5/Models/LabModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend5.Models
+{
+    public class LabModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите адрес")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Введите номер телефона")]              
+        [Display(Name = "Phones")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\-\d{2}]?)?[\d\-\d{2}]{6,11}$", ErrorMessage = "Недопустимый формат")] 
+        public string Phones { get; set; }
+    }
+}
